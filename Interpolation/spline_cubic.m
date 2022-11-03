66c5a880718e1e5c4d4f4f64f6c9b3e87a1404c4
Kenny Wu <110704880+kennyorn1@users.noreply.github.com>
2022-11-03 21:37:52 +0800

    Update spline_cubic.m

--- Interpolation/spline_cubic.m
+++ Interpolation/spline_cubic.m
@@ -7,13 +7,13 @@ plot(x,y,'o');
 hold on;
 %绘图节点
 xi=[0:0.01:3];
-%三次样条插值
-y_spline=interp1(x,y,xi,"spline");
-plot(xi,y_spline,'r');
-hold on;
 %分段三次插值
 y_cubic=interp1(x,y,xi,'cubic');
 plot(xi,y_cubic,'b');
+hold on;
+%三次样条插值
+y_spline=interp1(x,y,xi,"spline");
+plot(xi,y_spline,'r');
 hold on;
 %图例
 legend('插值点','三次样条插值','分段三次插值');
