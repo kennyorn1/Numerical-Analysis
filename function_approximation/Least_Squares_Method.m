4fef25267d507a3946f65a106d20bc19a980875e
Kenny Wu <110704880+kennyorn1@users.noreply.github.com>
2022-11-17 20:24:40 +0800

    Update Least_Squares_Method.m

--- function_approximation/Least_Squares_Method.m
+++ function_approximation/Least_Squares_Method.m
@@ -11,11 +11,10 @@ for i=1:1:n-1
     p=polyfit(x,y,i);
     yi=polyval(p,xi);
     plot(xi,yi);
-    hold on;
-   
+    hold on;   
 end 
 num=["拟合点"];
 for i=1:1:n-1
     num=[num,[num2str(i)+"次多项式"]];
 end
-legend(num)
+legend(num);
