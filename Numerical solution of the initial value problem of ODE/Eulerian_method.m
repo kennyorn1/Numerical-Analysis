86d1b7fb3cd990e31e7de5b3f94b1b4d10178bee
kennyorn1 <1079346331@qq.com>
2022-11-26 19:00:24 +0800

    Update Eulerian_method.m

--- Numerical solution of the initial value problem of ODE/Eulerian_method.m
+++ Numerical solution of the initial value problem of ODE/Eulerian_method.m
@@ -1,6 +1,6 @@
 xi=0;
 yi=1;
-h=0.001;
+h=0.1;
 y=[yi];
 x=[xi];
 while xi<1
@@ -12,3 +12,8 @@ while xi<1
 end
 plot(x,y);
 hold on;
+xx=0:0.01:1;
+yy=(1+2*xx).^(1/2);
+plot(xx,yy);
+hold on;
+legend('欧拉法','原函数');
