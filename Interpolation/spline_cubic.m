7f23d81e834dd8aa51a856fea3838f42f534ffef
kennyorn1 <1079346331@qq.com>
2022-12-02 13:23:00 +0800

    Update spline_cubic.m

--- Interpolation/spline_cubic.m
+++ Interpolation/spline_cubic.m
@@ -1,17 +1,21 @@
 clc;
 clear;
-%绘制原始数据点
+%绘制原函数
 x=0:0.01:3;
 y=sin(10*x.^2+3);
-plot(x,y,'o');
+plot(x,y,'g');
 hold on;
-xi=0:0.01:3;
+%设置插值节点
+xi=0:0.08:3;
+yi=sin(10*xi.^2+3);
+plot(xi,yi,'o');
+hold on;
 %分段三次插值
-y_cubic=interp1(x,y,xi,'cubic');
-plot(xi,y_cubic,'b');
+y_cubic=interp1(xi,yi,x,'cubic');
+plot(x,y_cubic,'r');
 hold on;
 %三次样条插值
-y_spline=interp1(x,y,xi,"spline");
-plot(xi,y_spline,'r');
+y_spline=interp1(xi,yi,x,"spline");
+plot(x,y_spline,'b');
 hold on;
-legend('原始数据','分段三次插值','三次样条插值');
+legend('原函数','节点','分段三次插值','三次样条插值');
