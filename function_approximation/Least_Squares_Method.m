749d5736f16bc73c4b36e01489b181b232770f08
kennyorn1 <1079346331@qq.com>
2022-12-02 13:22:57 +0800

    Update Least_Squares_Method.m

--- function_approximation/Least_Squares_Method.m
+++ function_approximation/Least_Squares_Method.m
@@ -5,6 +5,9 @@ xi=0:0.001:1;
 y=(x.^2-3*x+5).*exp(-5*x).*sin(x);
 plot(x,y,'o');
 hold on;
+yi=(xi.^2-3*xi+5).*exp(-5*xi).*sin(xi);
+plot(xi,yi);
+hold on;
 n=length(x);
 for i=1:1:n-1
     p=[];
@@ -13,8 +16,9 @@ for i=1:1:n-1
     plot(xi,yi);
     hold on;   
 end 
-num=["拟合点"];
+num=["拟合点",'原函数'];
 for i=1:1:n-1
     num=[num,[num2str(i)+"次多项式"]];
 end
 legend(num);
+text(0.2,0,'1到10次多项式');
