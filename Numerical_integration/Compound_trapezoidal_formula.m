1e7f04863e82613c6e4da21ce5134e431c13e9c2
kennyorn1 <1079346331@qq.com>
2022-12-02 13:23:02 +0800

    Update Compound_trapezoidal_formula.m

--- Numerical_integration/Compound_trapezoidal_formula.m
+++ Numerical_integration/Compound_trapezoidal_formula.m
@@ -5,13 +5,13 @@ x1=0:0.1:1;
 x1(1)=[];
 y1=sin(x1)./x1;
 %步长为0.001
-x2=0:0.001:1;
+x2=0:0.00001:1;
 x2(1)=[];
 y2=sin(x2)./x2;
 plot(x1,y1);
 hold on;
 plot(x2,y2);
 hold on;
-v1=trapz(x1,y1)
-v2=trapz(x2,y2)
+v1=trapz(x1,y1)%步长为0.1
+v2=trapz(x2,y2)%步长为0.001
 legend('步长为0.1','步长为0.001');
